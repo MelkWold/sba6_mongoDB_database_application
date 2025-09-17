a4e11f22e9d35ef3885f516f35e0571c67617190
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-09-17 11:49:25 -0400

    jobSeeker and employer schema added

--- modles/employerSchema.mjs
+++ modles/employerSchema.mjs
@@ -1,3 +1,25 @@
 import mongoose from "mongoose";
 
-export mongoose.model("Employers", employerSchema.mjs)
+const employerSchema =  new mongoose.Schema({
+    companyName: {type: String, required:true},
+    companyAddress: {
+        city: String, required:true,
+        state: String, required:true,
+        zipcode: String, required:true,
+        country: String, required:true},
+    companyType: {type: String,
+        enum: {
+            values: ["Private", "Public", "Government", "Nonprofit", "Education", "Healthcare", "International", "Other"],
+            required:true
+        }
+    } 
+})
+
+// Create indices for fast querying
+employerSchema.index({ companyName: 1 });
+employerSchema.index({ companyType: 1 });
+
+
+
+
+export default mongoose.model("Employers", employerSchema.mjs);
---
+++ modles/jobSeekerSchema.mjs
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const jobSeekerSchema =  new mongoose.Schema({
+    jobSeekserName: {type: String, required:true},
+    jobSeekserAddress: {
+        city: String, required:true,
+        state: String, required:true,
+        zipcode: String, required:true,
+        country: String, required:true},
+    jobSeekserField: {type: String,
+        enum: {
+            values: ["IT Support", "Fullstack Development", "Backend", "Frontend", "Teacher", "STEM", "Arts", "Other"],
+            required:true
+        }
+    } 
+})
+
+// Create indices for fast querying
+jobSeekerSchema.index({ jobSeekerName: 1 });
+jobSeekerSchema.index({ jobSeekerAddress: 1 });
+jobSeekerSchema.index({ jobSeekerField: 1 });
